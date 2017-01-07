624d63fa6979af1be7095f7abfbf2ab4406e1055
martin <xxm0703@abv.bg>
2017-01-07 18:54:51 +0200

    Still Not Working

--- obj.py
+++ obj.py
@@ -1,29 +1,69 @@
 import pygame
+import random
+
 pygame.init()
-BLACK = (0,0,0)
+R = random.randint(0, 255)
+G = random.randint(0, 255)
+B = random.randint(0, 255)
+COLOR = (R, G, B)
+print(COLOR)
+
+
 class Box(object):
     def __init__(self, surface, x, y, box_size):
         self.display = surface
+        self.box_size = box_size
         self.x = x
         self.y = y
-        self.box_size = box_size
-    def Draw(self):
-        return pygame.draw.rect(self.display,BLACK,[self.x*10,self.y*10,self.box_size,self.box_size])
 
-    def Param(self):
-        return self.y
-        return self.x
-class O(Box):
-    pass
-class I(Box):
-    pass
+    def Draw(self):
+        return pygame.draw.rect(self.display, COLOR, [self.x * 10, self.y * 10, self.box_size, self.box_size], 3)
+
+    def Struct(self):
+        pass
+
+    def Param(self, add_x=0, add_y=0):
+        return [self.x+add_x, self.y+add_y]
+
+
+class O(object):
+    def __init__(self, surface, x, y, box_size):
+        self.display = surface
+        self.box_size = box_size
+        self.x = x
+        self.y = y
+        self.list_cords = [O.Param(self, self.x, self.y), O.Param(self, self.x, self.y, add_x=1),
+                           O.Param(self, self.x, self.y, add_y=1), O.Param(self, self.x, self.y, add_x=1, add_y=1)]
+
+    def Struct(self):
+        for cord in self.list_cords:
+            Box(self.display,cord[0],cord[1],self.box_size).Draw()
+
+    def Param(self, x, y, add_x=0, add_y=0):
+        return [x+add_x, y+add_y]
+
+    def Ret_list(self):
+        return self.list_cords
+
+    def Ret_y(self):
+        return [self.y, self.y+1]
+
+
 class S(Box):
     pass
+
+
 class Z(Box):
     pass
+
+
 class L(Box):
     pass
+
+
 class J(Box):
     pass
+
+
 class T(Box):
     pass
--- tetris.py
+++ tetris.py
@@ -1,19 +1,18 @@
-import obj,pygame
+import obj
+import pygame
+
 pygame.init()
 box_size = 10
 wide = box_size*10
 high = box_size*20
 screen = pygame.display.set_mode((wide, high))
-board = [[0]*10]*20
 clock = pygame.time.Clock()
-objects = []
+pos = []
 obj_y = 0
 obj_x = 5
 ch_x = 0
 speed = 2
 IN = True
-cent = obj.Box(screen, obj_x, obj_y, box_size)
-objects.append(cent)
 while IN:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
@@ -28,19 +27,32 @@ while IN:
             speed = 2
         elif event.type == pygame.QUIT:
             IN = False
-    obj_y += 1
     obj_x += ch_x
+    obj_y += 1
     screen.fill((255,255,255))
-    cent = obj.Box(screen, obj_x, obj_y, box_size)
-    for fig in objects:
-        fig.Draw()
-    objects.append(cent)
-    if obj_y > 19:
-
+    cent = obj.O(screen,obj_x,obj_y, box_size)
+    #for cords in obj.O(screen,obj_x,obj_y,box_size).Ret_list()
+    pos.extend(cent.Ret_list())
+    for fig in pos:
+        obj.Box(screen,fig[0],fig[1],box_size).Draw()
+    print([x for x in cent.Ret_list() if x in pos[:-4]])
+    if 20 in cent.Ret_y() or [x for x in cent.Ret_list() if x in pos[:-5]]:
         obj_y = 0
+        print("hi")
     else:
-        del objects[-2]
+        print (pos)
+        del pos[-4:]
+        #del pos[-1]
+        #del pos[0])
+    line = []
+    for cord in pos:
+        line.append(cord[1])
+    for j in range(21):
+        if line.count(j) == 10:
+            i = 0
+            while i < 10:
+                del pos[line.index(j)]
+                i += 1
     pygame.display.update()
     clock.tick(speed)
-    print(board)
 pygame.quit()
