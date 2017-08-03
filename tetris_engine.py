6c29ce05e2c9cacc065647bdef539e1d8d1221d8
Martin Jordanov <xxm0703@abv.bg>
2017-08-03 15:00:02 +0300

    Speeding

--- tetris_engine.py
+++ tetris_engine.py
@@ -9,12 +9,11 @@ class Engine:
     def __init__(self, FPS, board, resolution):
         self.board = board
         self.MPF = 10 // FPS
-        self.speed = None
+        self.speed = self.MPF
         self.render = Graphic(board, resolution)
 
     def game_loop(self):
         while True:  # Game Loop
-            self.speed = self.MPF
             for x in range(4):
                 self.get_event()
                 self.render.draw()
@@ -32,7 +31,11 @@ class Engine:
                 if event.key == pygame.K_UP:
                     self.board.rotate_block()
                 elif event.key == pygame.K_DOWN:
-                    self.speed = self.MPF / 2
+                    self.speed = self.MPF * 4
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_DOWN:
+                    self.speed = self.MPF
 
 
 if __name__ == '__main__':
